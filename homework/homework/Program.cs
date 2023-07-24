2aea0898ff05b07659718542be88b785ed0e16ef
Elnur Gafarzade <138590291+1907elnur@users.noreply.github.com>
2023-07-24 23:33:44 +0400

    HOmework1

---
+++ homework/homework/Program.cs
@@ -0,0 +1,32 @@
+﻿#region task1
+//class Program
+//{
+
+//    static int onyeddiyeyaxin(int num1, int num2)
+//    {
+
+//        int eded1 = Math.Abs(num1 - 20);
+//        int eded2 = Math.Abs(num2 - 20);
+
+
+//        if (eded1 < eded2)
+//        {
+//            return num1;
+//        }
+//        else if (eded1 > eded2)
+//        {
+//            return num2;
+//        }
+//        else
+//        {
+//            return num1;
+//        }
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        int result = onyeddiyeyaxin(35, 10);
+//        Console.WriteLine(result); 
+//    }
+//}
+#endregion
